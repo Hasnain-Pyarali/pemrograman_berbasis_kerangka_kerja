13c658bec1a96ad4bccda7e1e43cb92bba934909
Hasnain-Pyarali <hasnain.pyarali@epitech.eu>
2024-10-06 04:04:27 +0700

    Blade Component
    Understood Laravel blade component to structure the code.

--- laravel_project/resources/views/contact.blade.php
+++ laravel_project/resources/views/contact.blade.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            Instagram: <a href="">Instagram</a>
-        </li>
-        <li>
-            X: <a href="">X</a>
-        </li>
-        <li>
-            Youtube: <a href="">Youtube</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h3 class="text-xl">Contact Page</h3>
+</x-layout>
